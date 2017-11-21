eabe0f2df9ec04fd7fd8123cdb8e6e91adb63f65
646720@lrstudents.org <646720@lrstudents.org>
2017-11-21 17:02:40 -0500

    imported old code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMain.java
@@ -0,0 +1,223 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by FTC-4390 on 11/16/2017.
+ */
+
+@Autonomous(name="MainAutonomous", group="Autonomous")  // @Autonomous(...) is the other common choice
+public class AutonomousMain extends LinearOpMode {
+
+    /* Declare OpMode members. */
+
+    private boolean hardCode = true;
+
+    private ElapsedTime runtime = new ElapsedTime();
+    //private DcMotor shooterLeft = null;
+    //private DcMotor shooterRight = null;
+    private DcMotor leftMotor1 = null;
+    private DcMotor leftMotor2 = null;
+    private DcMotor rightMotor1 = null;
+    private DcMotor rightMotor2 = null;
+    //private DcMotor sweeper = null;
+    //private DcMotor conveyor = null;
+    private CRServo leftArmServo = null;
+    private CRServo rightArmServo = null;
+    private int programState = 1;
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1368 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 3.8 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        // leftMotor  = hardwareMap.dcMotor.get("left motor");
+        // rightMotor = hardwareMap.dcMotor.get("right motor");
+
+        // eg: Set the drive motor directions:
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        leftMotor1 = hardwareMap.dcMotor.get("leftMotor1");
+        rightMotor1 = hardwareMap.dcMotor.get("rightMotor1");
+        leftMotor2 = hardwareMap.dcMotor.get("leftMotor2");
+        rightMotor2 = hardwareMap.dcMotor.get("rightMotor2");
+        leftArmServo = hardwareMap.crservo.get("leftArmServo");
+        rightArmServo = hardwareMap.crservo.get("rightArmServo");
+
+        //shooterLeft = hardwareMap.dcMotor.get("shooterLeft");
+        /*shooterRight = hardwareMap.dcMotor.get("shooterRight");
+        sweeper = hardwareMap.dcMotor.get("sweeper");
+        conveyor = hardwareMap.dcMotor.get("conveyor");
+*/
+
+        // eg: Set the drive motor directions:
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftMotor1.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor1.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor2.setDirection(DcMotor.Direction.FORWARD);
+        leftMotor2.setDirection(DcMotor.Direction.REVERSE);
+        runtime.reset();
+
+        if(hardCode == true) {
+            telemetry.addData("Status", "Resetting Encoders");    //
+            telemetry.update();
+            leftMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            idle();
+            leftMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            telemetry.addData("Path0", "Starting at %7d :%7d",
+                    leftMotor1.getCurrentPosition(),
+                    leftMotor2.getCurrentPosition(),
+                    rightMotor1.getCurrentPosition(),
+                    rightMotor2.getCurrentPosition());
+            telemetry.update();
+            encoderDrive(DRIVE_SPEED, 32.25, 32.25, 15);  // S1: Forward 47 Inches with 5 Sec timeout
+
+            hardCode = false;
+        }
+
+        // run until the end of the match (driver presses STOP)
+        if (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+            /*shooterLeft.setPower(0.37);
+            shooterRight.setPower(0.37);*/
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //conveyor.setPower(1);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            /*conveyor.setPower(0);
+            shooterLeft.setPower(0);
+            shooterRight.setPower(0);*/
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            leftMotor1.setPower(1);
+            rightMotor1.setPower(1);
+            leftMotor2.setPower(1);
+            rightMotor2.setPower(1);
+            try {
+                Thread.sleep(625);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            leftMotor1.setPower(0);
+            rightMotor1.setPower(0);
+            leftMotor2.setPower(0);
+            rightMotor2.setPower(0);
+            programState = 0;
+
+            requestOpModeStop();
+
+
+
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
+    }
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) throws InterruptedException {
+        int newLeftTarget;
+        int newRightTarget;
+
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = leftMotor1.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightMotor1.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newLeftTarget = leftMotor2.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightMotor2.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftMotor1.setTargetPosition(newLeftTarget);
+            rightMotor1.setTargetPosition(newRightTarget);
+            leftMotor2.setTargetPosition(newLeftTarget);
+            rightMotor2.setTargetPosition(newRightTarget);
+
+
+            // Turn On RUN_TO_POSITION
+            leftMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftMotor1.setPower(Math.abs(speed));
+            rightMotor1.setPower(Math.abs(speed));
+            leftMotor2.setPower(Math.abs(speed));
+            rightMotor2.setPower(Math.abs(speed));
+
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftMotor1.isBusy() && rightMotor1.isBusy())) {
+
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        leftMotor1.getCurrentPosition(),
+                        rightMotor1.getCurrentPosition());
+                telemetry.update();
+
+
+                // Allow time for other processes to run.
+                idle();
+            }
+
+
+            // Stop all motion;
+            leftMotor1.setPower(0);
+            rightMotor1.setPower(0);
+
+
+            // Turn off RUN_TO_POSITION
+            leftMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by FTC-4390 on 11/8/2017.
+ */
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public class TeleOp extends OpMode {
+    private DcMotor leftMotor1 = null;
+    private DcMotor rightMotor1 = null;
+    private DcMotor leftMotor2 = null;
+    private DcMotor rightMotor2 = null;
+    private DcMotor leftArmMotor = null;
+    private DcMotor rightArmMotor = null;
+    private DcMotor centralArmMotor = null;
+    private CRServo leftArmServo = null;
+    private Servo rightArmServo = null;
+
+    public void init() {
+        leftMotor1 = hardwareMap.dcMotor.get("leftMotor1");
+        rightMotor1 = hardwareMap.dcMotor.get("rightMotor1");
+        leftMotor2 = hardwareMap.dcMotor.get("leftMotor2");
+        rightMotor2 = hardwareMap.dcMotor.get("rightMotor2");
+        leftArmMotor = hardwareMap.dcMotor.get("leftArmMotor");
+        rightArmMotor = hardwareMap.dcMotor.get("rightArmMotor");
+        centralArmMotor = hardwareMap.dcMotor.get("centralArmMotor");
+        leftArmServo = hardwareMap.crservo.get("leftArmServo");
+        rightArmServo = hardwareMap.servo.get("rightArmServo");
+
+
+        leftMotor1.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor1.setDirection(DcMotor.Direction.FORWARD);
+        leftMotor2.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor2.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
+    public void loop() {
+        float throttleLeft = gamepad1.left_stick_y;
+        float throttleRight = gamepad1.right_stick_y;
+
+        //move the whole arm up or down
+        if (gamepad2.y) {
+            leftArmMotor.setPower(.5);
+            rightArmMotor.setPower(-.5);
+        } else if (gamepad2.a) {
+            leftArmMotor.setPower(-.2);
+            rightArmMotor.setPower(.2);
+        } else {
+            leftArmMotor.setPower(0);
+            rightArmMotor.setPower(0);
+        }
+
+        //move the whole arm in or out
+        if (gamepad2.dpad_up) {
+            centralArmMotor.setPower(.5);
+        } else if (gamepad2.dpad_down) {
+            centralArmMotor.setPower(-.5);
+        } else {
+            centralArmMotor.setPower(0);
+        }
+
+        //rotate the left arm in or out
+        if (gamepad2.left_bumper) {
+            leftArmServo.setPower(0.3);
+        } else if (gamepad2.left_trigger > 0) {
+            leftArmServo.setPower(-0.3);
+        } else {
+            leftArmServo.setPower(0);
+        }
+
+        //rotate the right arm in or out
+        if (gamepad2.right_bumper)
+            rightArmServo.setPosition(0.3);
+        else if (gamepad2.right_trigger > 0)
+            rightArmServo.setPosition(0);
+
+
+        throttleRight = Range.clip(throttleRight, -1, 1);
+        throttleLeft = Range.clip(throttleLeft, -1, 1);
+
+
+        throttleRight = (float) scaleInput(throttleRight);
+        throttleLeft = (float) scaleInput(throttleLeft);
+
+
+
+        leftMotor1.setPower(throttleLeft);
+        rightMotor1.setPower(throttleRight);
+        leftMotor2.setPower(throttleLeft);
+        rightMotor2.setPower(throttleRight);
+
+    }
+
+    double scaleInput(double dVal) {
+        double[] scaleArray = {0.0, 0.05, 0.10, 0.15, 0.17, 0.20, 0.25, 0.30,
+                0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00, 1.00};
+
+        //Original scaleInput value array
+
+           /*double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };*/
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+}
